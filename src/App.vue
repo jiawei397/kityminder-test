651e4d259017fa6b0c1d9b9e2955f875b11afe95
jw397 <jw397@126.com>
2020-02-24 10:06:34 +0800

    feat: 使用kityminder来展示

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div id="minder-container">
+
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  mounted () {
+    this.km = new window.kityminder.Minder({
+      renderTo: '#minder-container'
+    })
+    this.km.importJson({
+      root: {
+        data: {
+          text: 'test111'
+        },
+        children: [
+          { data: { text: '新闻\nsrc/module/node.j' } },
+          { data: { text: '网页' } },
+          { data: { text: '贴吧' } },
+          { data: { text: '知道' } },
+          { data: { text: '音乐' } },
+          {
+            data: {
+              text: '图片'
+            },
+            children: [
+              {
+                data: {
+                  text: 'abc'
+                }
+              }
+            ]
+          },
+          { data: { text: '视频' } },
+          { data: { text: '地图' } },
+          { data: { text: '百科', expandState: 'collapse' } }
+        ]
+      },
+      template: 'default'
+    })
+
+    window.km = this.km
+  }
+}
+</script>
+<style lang="stylus" scoped>
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 10px
+  height 800px
+
+#minder-container{
+  width 100%
+  height 100%
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import 'kity'
+import 'kityminder-core'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
